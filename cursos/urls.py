94bc35e5820025047d2bd25797f2fce4924d104f
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-30 17:06:26 -0300

    feat: alterando views

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,8 +4,10 @@ from .views import AvaliacoesAPIView, CursosAPIView, CursoAPIView,AvaliacaoAPIVi
 app = 'cursos'
 urlpatterns = [
     path('cursos/',CursosAPIView.as_view(), name='cursos'),
-    path('curso/<int:pk>',CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/',CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',AvaliacoesAPIView.as_view(), name='curso_avaliacao'),
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacao/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('avaliacao/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
 
--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Cursos, Avaliacao
 from .serializers import AvaliacaoSerializer, CursosSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CursosAPIView(generics.ListCreateAPIView):
@@ -15,7 +19,14 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+    
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    
       
