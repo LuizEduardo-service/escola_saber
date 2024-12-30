a5af5e24128bad748ceb3fb08e7937b34e707ba6
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-30 12:49:31 -0300

    feat: criando rotas

--- cursos/models.py
+++ cursos/models.py
@@ -34,4 +34,4 @@ class Avaliacao(Base):
         unique_together = ['email', 'curso']
 
     def __str__(self):
-        return f"{self.nome} avaliou o curso {self.curso} com nota {self.Avaliacao}"
+        return f"{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}"
