1406a6339b2b821d54e088cf6b3b211faa747daf
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-30 16:23:30 -0300

    feat: inclundo url

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path, include
-from .views import AvaliacoesAPIView, CursosAPIView
+from .views import AvaliacoesAPIView, CursosAPIView, CursoAPIView,AvaliacaoAPIView
 app = 'cursos'
 urlpatterns = [
     path('cursos/',CursosAPIView.as_view(), name='cursos'),
+    path('curso/<int:pk>',CursoAPIView.as_view(), name='curso'),
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacao/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
 
