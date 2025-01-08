b476e220b3be85c2e32a02fbf738dd9b89f02146
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-01-08 10:09:06 -0300

    feat: criando tests

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Cursos, Avaliacao
+from django.db.models import Avg
 
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
@@ -11,10 +12,15 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         model = Avaliacao
         fields = ['id', 'curso', 'nome', 'comentario', 'avaliacao', 'criacao', 'ativo']
 
+    def validacao_avaliacao(self, valor):
+        if valor in range(1,6):
+            return valor
+        return serializers.ValidationError('Avaliação deve ser entre 1 e 5')
+
 class CursosSerializer(serializers.ModelSerializer):
 
     avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
-
+    media_avaliacoes = serializers.SerializerMethodField()
     # gera um link dos dados gerados
     # avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name = 'avaliacao-detail')
     
@@ -23,4 +29,10 @@ class CursosSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cursos
-        fields = ['id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes']
+        fields = ['id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes', 'media_avaliacoes']
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return (media*2)/2
---
+++ tests/test_crud.py
@@ -0,0 +1,44 @@
+import requests
+from unittest import TestCase
+
+class TestCursos(TestCase):
+    headers = {'Authorization': 'Token seutoken'}
+    url_base_cursos = 'http://localhost:8000/api/v2/cursos/'
+
+    def test_get_cursos(self):
+        resposta = requests.get(url=self.url_base_cursos, headers=self.headers)
+        self.assertEqual(resposta.status_code, 200)
+
+    def test_get_curso(self):
+        resposta = requests.get(url=f'{self.url_base_cursos}2/', headers=self.headers)
+        self.assertEqual(resposta.status_code, 200)
+    
+    def test_post_curso(self):
+        novo = {
+            "titulo": "Novo curso1",
+            "url": "http://novocurso1.com"
+        }
+        resposta = requests.post(url=self.url_base_cursos, headers=self.headers, data=novo)
+        self.assertEqual(resposta.status_code, 201)
+        self.assertEqual(resposta.json()['titulo'], novo['titulo'])
+
+    
+    def test_put_curso(self):
+        atualizado = {
+            "titulo": "Curso atualizado1",
+            "url": "http://atualizado1.com"
+        }
+        resposta = requests.put(url=f'{self.url_base_cursos}3/', headers=self.headers, data=atualizado)
+        self.assertEqual(resposta.status_code, 200)
+    
+    def test_put_titulo_curso(self):
+        atualizado = {
+            "titulo": "Curso atualizado2",
+            "url": "http://atualizado2.com"
+        }
+        resposta = requests.put(url=f'{self.url_base_cursos}3/', headers=self.headers, data=atualizado)
+        self.assertEqual(resposta.json()['titulo'], atualizado["titulo"])
+
+    def test_delete_curso(self):
+        resposta = requests.delete(url=f'{self.url_base_cursos}2/', headers=self.headers)
+        self.assertEqual(resposta.status_code, 204)
